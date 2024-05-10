f64bb5f99f77a118b5d32c3902256da44b98a252
Ana Clara Ventura Silva <anaclaraventura11@gmail.com>
2024-05-10 16:32:11 -0300

    implementação do delete nos dados e desafio #2 do dia 10/05

--- src/components/ListaAnimais/ListaAnimais.jsx
+++ src/components/ListaAnimais/ListaAnimais.jsx
@@ -1,60 +1,69 @@
 import { useState } from 'react';
 import './ListaAnimais.css'
-import { useEffect} from 'react';
+import { useEffect } from 'react';
+import AnimalRequests from '../../fetch/AnimalRequests'
+import { FaTrash } from "react-icons/fa";
 
 function ListaAnimais() {
 
-    const [animais,setAnimais] = useState([]);
+    const [animais, setAnimais] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/listar-aves');
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar servidor');
-                }
-                const listaAnimais = await response.json();
-                setAnimais(listaAnimais);
-            } catch (error) {
-                console.error('Erro: ', error);
-            }
+            setAnimais(await AnimalRequests.listarAnimais());
+            console.log(animais);
         }
 
         fetchData();
     }, []);
 
+    const deletarAnimal = async (id) => {
+        const confirma = window.confirm(`Deseja deletar o animal com o id ${id}?`);
+        if(confirma){
+            if(await AnimalRequests.deletarAnimal(id)){
+                window.alert(`Animal deletado cm sucesso`)
+                window.location.reload();
+            }
+            else{
+                window.alert(`Erro ao deletar o animal`)
+            }
+        }
+    }
+
 
     return (
         <>
             <div className='pagina'>
-            <div className='alinhar'>
-            <h1>Lista de animais</h1>
-            <table className='tabela'>
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Idade</th>
-                        <th scope="col">Genero</th>
-                        <th scope="col">Envergadura</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {animais.length > 0 ? (
-                    animais.map((animal) => (
-                        <tr>
-                        <td>{animal.nomeanimal}</td>
-                        <td>{animal.idadeanimal}</td>
-                        <td>{animal.generoanimal}</td>
-                        <td>{animal.envergaduraanimal}</td>
-                    </tr>
-                    )
-                )): (
-                    <p>Carregando... Verifique se o servidor está funcionando</p>
-                )}
-                    
-                </tbody>
-            </table>
-            </div>
+                <div className='alinhar'>
+                    <h1>Lista de animais</h1>
+                    <table className='tabela'>
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Idade</th>
+                                <th scope="col">Genero</th>
+                                <th scope="col">Envergadura</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {animais ? (
+                                animais.map((animal) => (
+                                    <tr>
+                                        <td>{animal.nomeanimal}</td>
+                                        <td>{animal.idadeanimal}</td>
+                                        <td>{animal.generoanimal}</td>
+                                        <td>{animal.envergadura}</td>
+                                        <td onClick={() => deletarAnimal(animal.idanimal)}><FaTrash /></td>
+                                    </tr>
+                                )
+                                )) : (
+                                <p>Carregando... Verifique se o servidor está funcionando</p>
+                            )}
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
